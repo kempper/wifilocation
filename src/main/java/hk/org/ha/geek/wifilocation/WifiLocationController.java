562ea97b0b2c8839bb4608898bf87be28c26ea94
reohinho <reohinho@gmail.com>
2018-04-21 02:26:49 +0000

    update

--- src/main/java/hk/org/ha/geek/wifilocation/WifiLocationController.java
+++ src/main/java/hk/org/ha/geek/wifilocation/WifiLocationController.java
@@ -22,8 +22,61 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RestController
 public class WifiLocationController {	
     
+    @RequestMapping(value="/api/v1/poilist",method = RequestMethod.GET, headers="Accept=application/json")
+    public String getFullList(@RequestParam(value="isSameDayEvent") String isSameDayEvent) {        
+    String json = "";
+    	    	
+    	ObjectMapper mapper = new ObjectMapper();
+    	try {    
+    		
+    		List eventList = new ArrayList();
+    		HashMap poiMap = new HashMap();
+
+    		StringBuilder contentBuilder = new StringBuilder();
+    		try {
+
+    		    BufferedReader in = new BufferedReader(new FileReader("/home/pbrcadm/projects/poi_list.csv"));
+    		    BufferedReader in2 = new BufferedReader(new FileReader("/home/pbrcadm/projects/event_list.csv"));
+
+                POIBean bean = new POIBean();
+
+    		    String str;
+    		    while ((str = in.readLine()) != null) {
+    		        String[] poi = str.split(",");
+    		        poiMap.put(poi[0], poi);
+    		    }
+    		    
+    		    while ((str = in2.readLine()) != null) {
+    		        String[] event = str.split(",");
+                    String[] poi = (String[])poiMap.get(event[0]);
+                    bean.setRegionId(event[0]);
+                    bean.setPoiName(poi[1]);
+                    bean.setActions(event[1],event[2],event[3]);
+                    if(isSameDayEvent!=null&&isSameDayEvent.equals("T")) {
+                        if(isSameDayEvent.equals[event[3]) {
+                            eventList.add(bean);
+                        }
+                    }
+                    else {
+                        eventList.add(bean);
+                    }
+    		    }
+    		    
+    		    in.close();
+    		} catch (IOException e) {
+    		}
+    		
+    		json = mapper.writeValueAsString(eventList);
+    		
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+    	return json;
+    
+    }
+    
     @RequestMapping(value="/api/v1/poi",method = RequestMethod.GET, headers="Accept=application/json")
-    public String getTransactions(@RequestParam(value="id") String id) {        
+    public String getPoi(@RequestParam(value="id") String id) {        
         
     	String json = "";
     	    	
